69e0642ddda8d7b8db215a2a72a9412fbff6c300
liujx42 <liujx42@users.noreply.github.com>
2018-04-25 12:10:05 +0800

    mooc update

--- MOOC/2-university_rank.py
+++ MOOC/2-university_rank.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
 import requests
-import os
+from bs4 import BeautifulSoup
+import bs4
 
-url = "http://pic29.photophoto.cn/20131204/0034034499213463_b.jpg"
-path = os.getcwd()
-phname = path + '\\' + url.split('/')[-1]
-
-try:
-	uag = {'user-agent':'Chrome/66.0.3359.117'}
-	r = requests.get(url,headers=uag)
-	r.raise_for_status()
+def getHTMLText(url):
+	try:
+		r = requests.get(url, headers={'user-agent':'Chrome/66.0.3359.117'})
+		r.raise_for_status()
+		r.encoding = r.apparent_encoding
+		return r.text
+	except:
+		return ""
+		
+def fillUnivList(ulist, html):
+	soup = BeautifulSoup(html, 'html.parser')
+	for tr in soup.find('tbody').children:
+		if isinstance(tr, bs4.element.Tag):
+			tds = tr('td')
+			ulist.append([tds[0].string, tds[1].string, tds[3].string])
+			
+def printUnivList(ulist,num):
+	tplt = "{0:^10}\t{1:{3}^10}\t{2:^10}"
+	print(tplt.format('排名','学校名称','总分',chr(12288)))
+	for i in range(num):
+		u = ulist[i]
+		print(tplt.format(u[0],u[1],u[2],chr(12288)))
+		
+def main():
+	uinfo = []
+	url = "http://www.zuihaodaxue.com/zuihaodaxuepaiming2018.html"
+	html = getHTMLText(url)
+	fillUnivList(uinfo, html)
+	printUnivList(uinfo, 20)
 	
-	with open(phname,'wb') as f:
-		f.write(r.content)
-		f.close()
-		print('Sucessed')
-except:
-	print("Wrong")
-
+if __name__ == '__main__':
+	main()
+	
